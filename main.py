7566f8bf15976ba44814d2120ace5ea3edd6f4b3
bloodyblade <freesxsad23@mail.ru>
2024-02-08 02:29:17 +0300

    added new variable and function to generate hard password

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ capitalize_chrs = [chr.capitalize() for chr in chrs]
 nums = [str(digit) for digit in range(10)]
 spec_chrs = ["!", "#", "$", "%", "&", "(", ")", "*", "+", "/", ":", "<", "=", ">", "?", "@", "[", "]", "^", "_", "`", "{", "}", "~"]
 
+chrs_half_len = len(chrs) // 2
 
 def to_binary(num: int):
     s = ""
@@ -13,3 +14,12 @@ def to_binary(num: int):
         s = str(num%2) + s
         num //= 2
     return s
+
+def hard(length: int = 8, possible_words: str = None):
+    characters = chrs[:chrs_half_len] + nums
+    password = [random.choice(characters) for i in range(length)]
+    for index, letter in enumerate(password):
+        if letter.isalpha():
+            password[index] = letter.upper()
+            return "".join(password)
+
